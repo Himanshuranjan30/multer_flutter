85fe161a5f5ad75de9e6d13de1e219a6c68c2853
himanshu <himanshu@pop-os.localdomain>
2020-12-16 22:21:42 +0530

    multer implemented

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.get('/',(req, res) => {
  
  });
  
- app.use('/uploads',(req, res) =>express.static('/uploads/'))
+ app.use('/uploads', express.static(__dirname +'/uploads'));
  var storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'uploads')
